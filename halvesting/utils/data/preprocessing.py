b4b10e967d1c1cd4b857c1aa115e95f1d832968d
Madjakul <francis.kulumba@inria.fr>
2024-06-01 15:24:14 +0000

    Adding author parsing.

--- halvesting/utils/data/preprocessing.py
+++ halvesting/utils/data/preprocessing.py
@@ -7,6 +7,58 @@ from datetime import datetime
 from lxml.html import HtmlElement
 
 
+def _parse_authors(authors: List[HtmlElement]):
+    """Parses the authors into a uniform list of dict.
+
+    Parameters
+    ----------
+    authors: List[lxml.html.HtmlElement]
+        List of nodes of authors.
+
+    Returns
+    -------
+    authors_list: List[Dict[str, str]]
+        List of formated authors.
+    """
+    authors_list = []
+    for author in authors:
+        author_ = {}
+        author_["affiliations"] = []
+        for name in author.xpath(".//persname"):
+            author_["name"] = re.sub(                                       # Joins all the words constituing the name
+                r" +",
+                r" ",
+                "".join(name.itertext()).strip()
+            )
+        for e in author.xpath(".//idno | .//affiliation | .//email"):       # In the idno, affiliation or email balises
+            attrib = e.attrib
+            text = e.text
+            if "type" in attrib:
+                if attrib["type"] == "domain" or attrib["type"] == "idhal": # We don't want the mail domain nor the idhal
+                    continue
+                elif attrib["type"] == "md5":                               # The encrypted email adress
+                    author_[attrib["type"].lower()] = text
+                elif attrib["type"] == "halauthorid":                       # Then end of the halauthorid (identified author or not)
+                    author_["halauthorid"] = re.sub(
+                        r"(^.+)-(.+$)",
+                        r"\2",
+                        text
+                    )
+                else:
+                    # We only take the unique identifier for external ids (arxiv, viaf, ...)
+                    author_[attrib["type"].lower()] = re.sub(
+                        r"(^.+)((?<=\/)[^\/]+$)",
+                        r"\2",
+                        text
+                    )
+            else:
+                author_["affiliations"].append(
+                    re.sub(r"(^.+)-(.+$)", r"\2", attrib["ref"])
+                )
+        authors_list.append(author_)
+    return authors_list
+
+
 def extract_year(date_string: str):
     """Extracts a paper's production year.
 
@@ -72,6 +124,8 @@ def format_hal(data: HtmlElement):
 
         parsed_data = {}
         halid = match.xpath(".//idno[contains(@type,'halId')]")[0]
+        title = match.xpath(".//title")[0]
+        authors = match.xpath(".//titlestmt/author[contains(@role, 'aut')]")
         # The date is the produced year provided by HAL
         date = match.xpath(
             """.//editionstmt/edition[contains(@type, 'current')]
@@ -87,11 +141,13 @@ def format_hal(data: HtmlElement):
 
         parsed_data["halid"] = re.sub(r"(^.+)-(.+$)", r"\2", halid.text)
         parsed_data["lang"] = lang
+        parsed_data["title"] = title.text
         parsed_data["domain"] = domain
         parsed_data["timestamp"] = datetime.now().strftime("%Y/%m/%d %H:%M:%S")
         parsed_data["year"] = extract_year(date.text)
         # Sometimes, several PDFs are submitted by a depositor. We assume the
         # first one being the main publication.
         parsed_data["url"] = pdf[0].attrib["target"]
+        parsed_data["authors"] = _parse_authors(authors)
         new_data.append(parsed_data)
     return new_data
